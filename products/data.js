804351af13ad3a27d7a30e04c04bc4a5a3876853
Fiona Loo <fionaloo69@gmail.com>
2024-02-07 19:23:35 +0000

    Added edit task function

--- products/data.js
+++ products/data.js
@@ -1,1 +1,38 @@
 // Model Scripts - Data Processing
+
+
+function createTask() {
+
+}
+
+/**
+ * Updates an existing task's with new input from users
+ * @param {object} r Each item's infomation (Returns all)
+ * @param {*} data Data from data.json file
+ * @param {Array} editedItem Edited item's information (Returns only 1)
+ * @param {Array} header Header of item table
+ */
+function updateTask(id, data, editedItem, header) {
+  let editedTask = null;
+  for (let d of data.products) {
+    if (d.id == id) {
+      editedTask = d;
+      break;
+    }
+  }
+
+  for (i = 1; i < editedItem.length; i++) {
+    editedTask[header[i].toLowerCase()] = editedItem[i-1];
+  }
+}
+
+/**
+ * Deletes an existing task
+ * @param {object} r Each product's infomation
+ * @param {*} data Data from data.json file
+ */
+function deleteTask(r, data) {
+  alert(
+    "process delete clicked"
+  )
+}
--- products/script.js
+++ products/script.js
@@ -2,47 +2,42 @@
 document.addEventListener("DOMContentLoaded", async function () {
   // SCRIPT FOR INVENTORY ITEMS
   // Create Inventory Items Header
-  const response = await axios.get("/../data.json");
-  let inventoryHeader = ["SKU", "Image", "Name", "Quantity", "Category", "Warehouse", "Availability", "Settings"];
+  async function main() {
+    const response = await axios.get("/../data.json");
+    let inventoryHeader = ["ID", "Image", "Name", "Quantity", "Category", "Warehouse", "Availability", "Settings"];
 
-  createInvHeader(response);
-  // CRUD FUNCTIONS
-  displayProducts();
+    // CRUD FUNCTIONS
+    displayTask(inventoryHeader, response.data);
+  }
 
-  function createInvHeader(response) {
+  async function displayTask(header, data) {
     const inventoryList = document.querySelector("#inventory-list");
-    const ulElement = document.createElement("ul");
-    ulElement.setAttribute("id", "inventory-header");
-    inventoryList.appendChild(ulElement);
-    for (let i of inventoryHeader) {
-      const liElement = document.createElement("li");
-      if (i == "Name") {
-        liElement.classList.add("inventory-name");
-      }
-      liElement.innerHTML = i;
-      ulElement.appendChild(liElement);
-      }
-    }
-
-  async function displayProducts() {
-    const inventoryList = document.querySelector("#inventory-list");
-    for (let r of response.data.products) {
+    inventoryList.innerHTML = "";
+    createHeader(header);
+    for (let r of data.products) {
       // Creates an unordered list after the list header with a class of "inventory item"
       const ulElement = document.createElement("ul");
       ulElement.classList.add("inventory-item");
       inventoryList.appendChild(ulElement);
       // Add list items into the created unordered list items
-      for (let i of inventoryHeader) {
+      for (let i of header) {
         const liElement = document.createElement("li");
         if (i == "Image") {
-          console.log(r[i.toLowerCase()]);
           liElement.innerHTML = `<img class="inventory-image" src="${r[i.toLowerCase()]}"/>`
         } else if (i == "Settings") {
-          liElement.classList.add("inventory-settiings");
+          liElement.classList.add("inventory-settings");
           editButton = document.createElement("button")
           editButton.innerHTML = "<i class='bx bx-edit'></i>"
+          editButton.classList.add("edit-button");
+          editButton.addEventListener("click", function(){
+            processEditTask(r, data, header);
+          })
           deleteButton = document.createElement("button")
           deleteButton.innerHTML = "<i class='bx bx-trash'></i>"
+          deleteButton.classList.add("delete-button");
+          deleteButton.addEventListener("click", function(){
+            processDeleteTask(r, data);
+          })
           liElement.append(editButton, deleteButton)
         } else {
           if (i == "Name") {
@@ -54,6 +49,55 @@ document.addEventListener("DOMContentLoaded", async function () {
         }
       }
     }
+
+    function createHeader(header) {
+      const inventoryList = document.querySelector("#inventory-list");
+      const ulElement = document.createElement("ul");
+      ulElement.setAttribute("id", "inventory-header");
+      inventoryList.appendChild(ulElement);
+      for (let i of header) {
+        const liElement = document.createElement("li");
+        if (i == "Name") {
+          liElement.classList.add("inventory-name");
+        }
+        liElement.innerHTML = i;
+        ulElement.appendChild(liElement);
+        }
+      }
+
+    /**
+     * UI when users click the edit button
+     * @param {object} task A task's infomation
+     * @param {*} data Data from data.json file
+     */
+    function processEditTask(task, data, header) {
+      editedItem = []
+      for (let h of header) {
+        if (h != "ID" && h != "Settings") {
+          const editedField = prompt(`Enter the new ${h.toLowerCase()}: `, task[h.toLowerCase()]);
+          editedItem.push(editedField);
+        }
+      } 
+
+      updateTask(task.id, data, editedItem, header);
+      displayTask(header, data);
+      
+    }
+
+    /**
+     * UI when users click the delete button
+     * @param {object} task A task's infomation
+     * @param {*} data Data from data.json file
+     */
+    function processDeleteTask(task, data) {
+      const confirmDelete = confirm(`Are you sure that you want to delete ${task.name}?`)
+      if (confirmDelete) {
+        deleteTask(task.id, data);
+      }
+    }
+
+    await main();
+
   }
 )
 
